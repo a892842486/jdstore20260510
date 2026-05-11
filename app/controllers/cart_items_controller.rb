d99daef46123d01dbc89bd7ff4e9f33acb37ede7
Bamu <a892842486@github.com>
2026-05-11 17:16:26 +0800

    feat(cart): add cart validation and stock checks

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,6 +12,12 @@ class CartItemsController < ApplicationController
   def update
     @cart = current_cart
     @cart_item = @cart.cart_items.find(params[:id])
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+      flash[:notice] = "成功變更數量"
+    else
+      flash[:warning] = "庫存不足"
+    end
     @cart_item.update(cart_item_params)
     redirect_to carts_path
   end
