db6a72a2e07e51f6d31107b9a965d1ae70461198
Bamu <a892842486@github.com>
2026-05-11 16:30:16 +0800

    feat(cart): show cart item count in navbar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,11 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-  end 
+  end
+
+  def add_to_cart
+    product = Product.find(params[:id])
+    current_cart.add_product_to_cart(product)
+    redirect_back fallback_location: products_path, notice: "成功加入購物車"
+  end
 end
