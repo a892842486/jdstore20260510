9a62e6ad90bc136d523221d5de3c8a9aa75ded07
Bamu <a892842486@github.com>
2026-05-15 12:34:10 +0800

    feat(mail): setup order mailer and development email preview

--- Gemfile
+++ Gemfile
@@ -56,12 +56,13 @@ group :development, :test do
   gem "rubocop-rails-omakase", require: false
 end
 
- gem 'devise'
- gem 'aasm'
+ gem "devise"
+ gem "aasm"
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem "letter_opener"
 end
 
 group :test do
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,16 +1,29 @@
-def apply_cancel(order)
-  setup_order(order)
+class OrderMailer < ApplicationMailer
+  def apply_cancel(order)
+    setup_order(order)
 
-  mail(
-    to: "jdstore@gmail.com",
-    subject: "[JDStore] 用戶 #{@user.email} 申請取消訂單 #{@order.token}"
-  )
-end
+    mail(
+      to: "jdstore@gmail.com",
+      subject: "[JDStore] 用戶 #{@user.email} 申請取消訂單 #{@order.token}"
+    )
+  end
 
-private
+  def notify_order_placed(order)
+    @order = order
+    @user = order.user
+    @product_lists = order.product_lists
 
-def setup_order(order)
-  @order = order
-  @user  = order.user
-  @product_lists = order.product_lists
+    mail(
+      to: @user.email,
+      subject: "[JDstore] 訂單成立通知 - #{order.token}"
+    )
+  end
+
+  private
+
+  def setup_order(order)
+    @order = order
+    @user  = order.user
+    @product_lists = order.product_lists
+  end
 end
