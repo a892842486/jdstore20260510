3ca8d51e5ae2ee342feed7458090e27472b881f9
Bamu <a892842486@github.com>
2026-05-20 10:59:23 +0800

    feat(i18n): add locale configuration and switching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   helper_method :current_cart
+  before_action :set_locale
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
@@ -20,5 +21,19 @@ class ApplicationController < ActionController::Base
       session[:cart_id] = cart.id
       cart
     end
+  end
+
+  private
+
+  def set_locale
+    if params[:locale].present? && I18n.available_locales.include?(params[:locale].to_sym)
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale]&.to_sym || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -24,4 +24,7 @@ module Jdstore
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+
+config.i18n.available_locales = [:"zh-TW", :en]
+config.i18n.default_locale = :"zh-TW"
 end
